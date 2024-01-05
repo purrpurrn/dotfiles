0c3e71a49f64322882d655f4ca6b72596668aa35
purrpurrn <scrcpynovideoaudiocodecraw+hdmzwl@gmail.com>
2024-01-05 18:16:41 +0100

    added swap

--- hosts/nos/default.nix
+++ hosts/nos/default.nix
@@ -14,11 +14,20 @@
   security.polkit.enable = true;
 
   boot.kernelPackages = pkgs.linuxPackages_zen;
+  
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.editor = false; # set to false out of security reasons
   boot.loader.timeout = 1;
   boot.loader.efi.canTouchEfiVariables = true; # Installation process is allowed to modify EFI boot variables.
-  boot.binfmt.emulatedSystems = ["aarch64-linux"];
+
+  boot.binfmt.emulatedSystems = ["aarch64-linux"]; # arm64 emulation
+  
+  # Swap
+  swapDevices = [{
+    device = "/dev/nvme0n1p5";
+    randomEncryption.enable = true;
+  }];
 
 
   # Bluetooth
---
+++ users/mew/pwd.nix
@@ -0,0 +1,5 @@
+{ config, pkgs, ... }: {
+  config = {
+    users.users.mew.hashedPassword = "$y$j9T$97zxsCPW4R2pnFPZCCu7k.$Qhs6.3RbTU9tkNbY6X34D3x766B8mAciFVeoVQvtIH2";
+  };
+}
