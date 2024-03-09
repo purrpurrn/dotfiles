7cdebc0d7f6931d03f1e709bad3c7b5da7931ecd
purrpurrn <scrcpynovideoaudiocodecraw+hdmzwl@gmail.com>
2024-03-09 18:27:12 +0100

    added icon to discord chromium wrapper
    Former-commit-id: 5d5b3a5cb297fe0da5930d3daaeb282f39a2d45d [formerly a502cd88acefcb2dd0a5070684a989daabb18731]
    Former-commit-id: 2d2322023d06a58d9bd90f0d716fee1460445414

--- home/mew/applications/discord/default.nix
+++ home/mew/applications/discord/default.nix
@@ -3,6 +3,7 @@
     xdg.desktopEntries.discord = {
       name = "${pkgs.discord.pname}";
       exec = "${pkgs.chromium}/bin/chromium --hide-scrollbars --app=https://discord.com/app";
+      icon = ./discord.svg;
       genericName = "${pkgs.discord.meta.description}";
       categories = [ "Network" "InstantMessaging" ];
       mimeType = [ "x-scheme-handler/discord" ];
