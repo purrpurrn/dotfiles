b26baa1df6a949a935424605c2936010b732c7fd
purrpurr <142632643+purrpurrn@users.noreply.github.com>
2024-05-19 19:08:18 +0200

    Update default.nix
    Former-commit-id: 70a50da33a28b6c9d9629ae9dc30aa1092fa6455 [formerly 6548f88fd8a996743bb9aac814ef2f63741abb45] [formerly c19bc35dbb087b36ab012ece4453601d24620075 [formerly fbe5163f52914803ac7cc106141a3e9117878c19]]
    Former-commit-id: ba93fe15cbbc21c6d11e37fe1b2d6bfd8c43f4c5 [formerly 251c24395e3d48d8ca971421f1a042021991642d]
    Former-commit-id: a88d9e962a4e21d41b2045cecd276b3751e4a988

--- devices/15s-eq0564ng/default.nix
+++ devices/15s-eq0564ng/default.nix
@@ -7,6 +7,9 @@
     ./bootloader.nix
   ];
 
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.kernelModules = [ "kvm-amd" "ddcci-driver-linux" ];
+
   # Audio
   boot.extraModprobeConfig = ''
     options snd-hda-intel patch=hda-jack-retask.fw
