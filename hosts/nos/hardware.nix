c65d0e195ed107e672b674554f15d9e2a3eb8bd1
purrpurr <142632643+purrpurrn@users.noreply.github.com>
2024-05-19 19:07:46 +0200

    Update hardware.nix
    Former-commit-id: e19dad5de513543c28acfa9526de3e144254f902 [formerly c0836c30a4538b34a93ad2da172fca705132430d] [formerly 29ec2843a5c8db0ef5e335d290507abb20f62f10 [formerly b883537a7bc61f58da16f63800dfba2970c95995]]
    Former-commit-id: 033d478f18ae90359e64539b16ee9ed5b218bb7b [formerly f18b22299d07017feb61f6a85a6b70c3525801c8]
    Former-commit-id: 5cbce170662f94fead213837005d53142050b047

--- hosts/nos/hardware.nix
+++ hosts/nos/hardware.nix
@@ -3,10 +3,6 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
-  boot.kernelModules = [ "kvm-amd" "ddcci-driver-linux" ];
-  boot.kernelPackages = pkgs.linuxPackages; # Kernel
-
   # Bluetooth
   hardware.bluetooth = {
     enable = true;
@@ -22,61 +18,4 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
-  # Swap
-  swapDevices = [{
-    device = "/dev/nvme0n1p5";
-    randomEncryption.enable = true;
-  }];
-
-  nixpkgs.hostPlatform = "x86_64-linux";
-
-  # impermanence 
-  fileSystems."/" =
-    { device = "none";
-      fsType = "tmpfs";
-      options = [ "defaults" "size=2G" "mode=755" ];
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/5304-83E9";
-      fsType = "vfat";
-    };
-
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/2c39a04e-afcf-4e5c-bd1f-d0aebca282a9";
-      fsType = "btrfs";
-      neededForBoot = true;
-    };
-
-  fileSystems."/etc/nixos" =
-    { device = "/nix/persist/etc/nixos";
-      fsType = "none";
-      options = [ "bind" ];
-    };
-
-  fileSystems."/home/mew/.ssh" =
-    { device = "/nix/persist/home/mew/.ssh";
-      fsType = "none";
-      depends = [ "/nix/persist" ];
-      options = [ "bind" ];
-    };
-
-  fileSystems."/etc/sshd" =
-    { device = "/nix/persist/etc/sshd";
-      fsType = "none";
-      depends = [ "/nix/persist" ];
-      options = [ "bind" ];
-    };
-
-  fileSystems."/var/log" =
-    { device = "/nix/persist/var/log";
-      fsType = "none";
-      options = [ "bind" ];
-    };
-  fileSystems."/var/lib/syncthing" =
-    { device = "/home/mew/tests";
-      fsType = "none";
-      options = [ "bind" ];
-    };
 }
